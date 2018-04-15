a8e494faa8f2ab1ba90649fb662710e99bbcebbd
Sherina Hung <31707212+sherina0922@users.noreply.github.com>
2018-04-15 17:15:55 -0500

    Add files via upload
    Maze generated.

---
+++ final-project-sherina0922-master/src/ofApp.h
@@ -0,0 +1,40 @@
+//ofApp.h
+#pragma once
+
+#include "ofMain.h"
+#include "maze.hpp"
+#include "cameraTracking.hpp"
+
+#define WIDTH 41
+#define HEIGHT 21
+
+class ofApp : public ofBaseApp{
+
+public:
+    void setup();
+    void update();
+    void draw();
+
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+
+    bool game_started = false;
+    
+    // Computer camera inputs
+    bool USE_CAMERA_INPUT = false; //TRUE if player decides to use camera input to move; FALSE if otherwise and by default
+    ofVideoGrabber comp_camera; // Computer Camera
+
+    // 3D view camera
+    ofEasyCam view_camera;
+
+
+};
