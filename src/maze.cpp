680a86fefd15f6ae751ba9a0fd485c9cd0a5e5f2
Sherina Hung <31707212+sherina0922@users.noreply.github.com>
2018-04-19 15:56:57 -0500

    Created game ended message

--- src/maze.cpp
+++ src/maze.cpp
@@ -6,38 +6,50 @@
 //
 
 #include "../src/maze.h"
+#include <cmath>
 //using namespace maze;
 
-int current_posX, current_posY; //the player's current position coordinates (in maze)
-int exit_posX, exit_posY; //the postion coordinates of the exit/goal
-//char maze_structure[WIDTH][HEIGHT];
-int mode; //level of difficulty which determines visibility
-bool mode_chosen; //TRUE if mode is already determined; FALSE if not chosen yet
 
-//static char maze_structure[WIDTH][HEIGHT];
+void Maze::SetMode(int new_mode) {
+    mode = new_mode;
+}
+
+void Maze::SetGameEnded(bool new_status) {
+    game_ended = new_status;
+}
 
 void Maze::DrawMaze() {
     
+    if (game_ended) {
+        ofBackground(FULL_COLOR,FULL_COLOR,FULL_COLOR);
+        ofSetColor(0, 0, 0);
+        ofDrawBitmapString("CONGRATULATIONS!\nYOU WIN!", 0, 0);
+        return;
+    }
+
     for (int current_y = 0; current_y < HEIGHT; current_y++) {
         for (int current_x = 0; current_x < WIDTH; current_x++) {
-            if (Maze::maze_structure[current_x][current_y] == '#') {
-                 ofPushMatrix();
-                 ofTranslate(current_x*5-100,-current_y*5+50,0);
-                 ofSetColor(63.0,63.0,63.0); //Set to TRANSPARENT GRAY
-                 ofFill();
-                 ofDrawBox(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE);
-                 ofNoFill();
-                 ofSetColor(0);
-                 ofDrawBox(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE);
-                 ofPopMatrix();
-             }
+            if (maze_structure[current_x][current_y] == '#' &&
+                std::abs(current_x - current_posX) < mode &&
+                std::abs(current_y - current_posY) < mode) {
+                
+                ofPushMatrix();
+                ofTranslate(current_x * CUBE_SIZE - 100, -current_y * CUBE_SIZE + 50,0);
+                ofSetColor(63.0,63.0,63.0); //Set to TRANSPARENT GRAY
+                ofFill();
+                ofDrawBox(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE);
+                ofNoFill();
+                ofSetColor(0);
+                ofDrawBox(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE);
+                ofPopMatrix();
+            }
         }
     }
     
     //draw player box
     ofPushMatrix();
-    ofTranslate(current_posX*5-100,-current_posY*5+50,0);
-    ofSetColor(255,0.0,0.0); //Set to RED
+    ofTranslate(current_posX * CUBE_SIZE - 100, -current_posY * CUBE_SIZE + 50,0);
+    ofSetColor(FULL_COLOR,0.0,0.0); //Set to RED
     ofFill();
     ofDrawBox(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE);
     ofNoFill();
@@ -47,8 +59,8 @@ void Maze::DrawMaze() {
     
     //draw goal box
     ofPushMatrix();
-    ofTranslate(exit_posX*5-100,-exit_posY*5+50,0);
-    ofSetColor(0.0,255.0,0.0); //Set to GREEN
+    ofTranslate(END_X * CUBE_SIZE - 100, -END_Y * CUBE_SIZE + 50,0);
+    ofSetColor(0.0,FULL_COLOR,0.0); //Set to GREEN
     ofFill();
     ofDrawBox(CUBE_SIZE, CUBE_SIZE, CUBE_SIZE);
     ofNoFill();
@@ -59,7 +71,7 @@ void Maze::DrawMaze() {
 
 void Maze::MazeSetup() {
     //Maze setup: Maze below derived from https://www.youtube.com/watch?v=9Ozu-B2HLY4
-    unsigned char maze2[HEIGHT][WIDTH] = {
+    unsigned char maze_template[HEIGHT][WIDTH] = {
         {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
         {'#',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
         {'#','#','#',' ','#','#','#',' ','#',' ','#',' ','#','#','#',' ','#','#','#',' ','#','#','#','#','#',' ','#',' ','#',' ','#','#','#','#','#','#','#',' ','#',' ','#'},
@@ -79,16 +91,20 @@ void Maze::MazeSetup() {
         {'#',' ','#',' ','#','#','#',' ','#',' ','#','#','#','#','#','#','#','#','#','#','#',' ','#',' ','#','#','#',' ','#',' ','#',' ','#','#','#',' ','#','#','#',' ','#'},
         {'#',' ','#',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ','#',' ','#',' ',' ',' ','#',' ',' ',' ',' ',' ','#'},
         {'#',' ','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#',' ','#',' ','#','#','#','#','#','#','#',' ','#','#','#','#','#','#','#'},
-        {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'}, //E was right before # symble
+        {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'}, //E was right before # symbol
         {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
         
     };
     
     for(int y = 0 ; y < HEIGHT ; y++){
         for(int x = 0 ; x < WIDTH ; x++){
-            maze_structure[x][y] = maze2[y][x];
+            maze_structure[x][y] = maze_template[y][x];
         }
     }
+    
+    //Hard set current position and exit position
+    current_posX = START_X;
+    current_posY = START_Y;
 }
 
 void Maze::MazeKeyPressed(const char key) {
@@ -120,5 +136,25 @@ void Maze::MazeKeyPressed(const char key) {
                 current_posX++;
             }
             break;
+            
+        /*case 'e':
+            //Set difficulty to EASY
+            mode = WIDTH;
+            break;
+            
+        case 'm':
+            //Set difficulty to MEDIUM
+            mode = 5;
+            break;
+            
+        case 'h':
+            //Set difficulty to HARD
+            mode = 2;
+            break; */
+            
+    }
+    if (current_posX == END_X && current_posY == END_Y) {
+        Maze::SetGameEnded(true);
     }
 }
+
