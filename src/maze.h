fad11869a25048cb3f8a0a05e2d46028f2f63e3f
Sherina Hung <31707212+sherina0922@users.noreply.github.com>
2018-04-21 20:31:35 -0500

    movement with camera works

--- src/maze.h
+++ src/maze.h
@@ -20,20 +20,28 @@
 #define END_Y 19
 #define FULL_COLOR 255
 
+/*
 static char maze_structure[WIDTH][HEIGHT];
 static int current_posX, current_posY; //the player's current position coordinates (in maze)
 static int mode; //level of difficulty which determines visibility
 static bool mode_chosen; //TRUE if mode is already determined; FALSE if not chosen yet
 static bool game_ended = false;
-
+ */
 class Maze {
     public :
     
-    static void SetMode(int new_mode);
-    static void SetGameEnded(bool new_status);
-    static void DrawMaze();
-    static void MazeSetup();
-    static void MazeKeyPressed(const char key);
+    char maze_structure[WIDTH][HEIGHT];
+    int current_posX, current_posY; //the player's current position coordinates (in maze)
+    int mode; //level of difficulty which determines visibility
+    bool mode_chosen; //TRUE if mode is already determined; FALSE if not chosen yet
+    bool game_ended = false;
+    
+    void SetMode(int new_mode);
+    void SetGameEnded(bool new_status);
+    void DrawMaze();
+    void MazeSetup();
+    void MazeKeyPressed(const char key);
+    void CameraMovePosition(int camera_current_x, int camera_current_y);
     
 };
 
